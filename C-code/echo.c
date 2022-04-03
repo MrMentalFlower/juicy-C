2486df52e75e58ca673a02910f0aacf0d86dc36a
flower <mr.mentalflower@gmail.com>
2022-04-02 22:37:59 -0400

    added complexity to echo.

--- C-code/echo.c
+++ C-code/echo.c
@@ -15,6 +15,7 @@ This program is to create a simple clone of the echo command while using case sw
 **************************************************************/
 #include <stdio.h>
 #include <unistd.h>
+#include <ctype.h>
 
 //args function argv needs to be ** which is a pointer to pointer
 void args(int argc,char **argv,int opt);
@@ -25,6 +26,8 @@ int main(int argc, char *argv[]) {
   int opt;
   //calls the args function.
   args(argc, argv, opt);
+
+  //prints dog if there are no arguments but there is text after the command.
 }
 
 
@@ -34,27 +37,45 @@ void args(int argc,char **argv,int opt){
   what commands are going to be recognized by getopt and the :f: defines input
   for f.
   */
-    while((opt = getopt(argc, argv, ":f:hv"))!= -1){
+    while((opt = getopt(argc, argv, ":c:hv"))!= -1){
       switch (opt) {
-        case 'f':
-          printf("%s\n", optarg);
+        case 'c':
+          int i;
+          char chr;
+            while(optarg[i]){
+              chr = optarg[i];
+              printf("%c", toupper(chr));
+              i++;
+            }
+          printf("\n");
           break;
 
         case 'h':
-          printf("This program is a clone of the echo command\n");
+          printf("This program is a clone of the echo command.\n"
+                  "To use this command you can pass straight text or pass options.\n"
+                  "All text should be put into quotations\n"
+                  "\n"
+                  "-c          Capitalizes all letters inputed.\n"
+                  "-h          Brings you to this prompt.\n"
+                  "-v          Shows the version\n");
           break;
 
-        case'v':
+        case 'v':
           printf("Version:0.01\n");
           break;
 
         case ':':
-          printf("Option needs a value\n");
+          printf("Value required\n");
           break;
 
         case '?':
-          printf("unknown option: %c\n", optopt);
+          printf("type -h to get help\n");
           break;
       }
     }
+
+    if(argc == 1){
+      printf("type -h to get help\n");
+    }
+
 }
