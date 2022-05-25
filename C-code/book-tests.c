b0766357b5d506071dce1c246cce5eba9129ebec
flower <mr.mentalflower@gmail.com>
2022-05-25 15:43:33 -0400

    asd

--- C-code/book-tests.c
+++ C-code/book-tests.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 
+char *hi(){
+  char *x = "hi";
 
-int main()
-{
-  int x;
-  while((x = fgetc(FILE *stream))){
-    printf("x = %c", x);
-  };
+  return x;
+}
+
+char *hello(){
+  char *x = "hello";
+
+  return x;
+}
+
+char *nope(){
+  char *x = "nope";
+
+  return x;
+}
+
+
+
+int main(void) {
+  int x = 0;
+
+  char *funray[100] = {hi(), hello(), nope()};
+
+  printf("%s\n", funray[x]);
+
+
+  return 0;
 }
